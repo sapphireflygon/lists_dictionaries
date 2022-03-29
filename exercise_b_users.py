abd1a45f55b3c821737892c7e9b6313f47ee8e8d
Jennifer Lane <jennifer.lane610@gmail.com>
2022-03-29 16:55:37 +0100

    exercise b q4 complete.

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -61,6 +61,7 @@ print(users["Erik"]["home_town"])
 # 3. Get the list of Erik's lottery numbers
 print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
+print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
