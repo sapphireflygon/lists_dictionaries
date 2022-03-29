cb85c5c8f4dbb6bcdad66193949d278bcf326405
Jennifer Lane <jennifer.lane610@gmail.com>
2022-03-29 16:29:59 +0100

    exercise a q8 complete.

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -15,5 +15,6 @@ del stops[2]
 #7. Print the number of stops there are in the list
 print(len(stops))
 #8. Sort the list alphabetically
+stops.sort()
 #9. Reverse the positions of the stops in the list
 #10 Print out all the stops using a for loop
