a2ff7c3f7ee6b3e44a7d101f0dec4564472284f8
Jennifer Lane <jennifer.lane610@gmail.com>
2022-03-29 17:02:32 +0100

    exercise b q5 complete.

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -63,6 +63,7 @@ print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
+print(users["Erik"]["lottery_numbers"][2])
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
